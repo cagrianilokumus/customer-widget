8ba758d7d047a6761aefa0e744df221bbafe1f8d
Çağrı Anıl Okumuş <114337825+cagrianilokumus@users.noreply.github.com>
2025-05-09 15:01:32 +0300

    Update widget.js

--- widget.js
+++ widget.js
@@ -477,54 +477,54 @@
     wrap.innerHTML = html;
     document.body.appendChild(wrap.firstElementChild);
 
-    function setupClick(){
-        $(".customer-services").on("click", function(){
-            $(this).toggleClass("active");
-        });
-    }
-
-    // (b) Typewriter sınıfı
-    var TxtType = function(el, toRotate, period){
+    $(".customer-services").on("click", function () {
+        $(this).toggleClass("active");
+    });
+    var TxtType = function (el, toRotate, period) {
         this.toRotate = toRotate;
         this.el = el;
         this.loopNum = 0;
-        this.period = parseInt(period,10) || 2000;
-        this.txt = '';
-        this.isDeleting = false;
+        this.period = parseInt(period, 10) || 2e3;
+        this.txt = "";
         this.tick();
+        this.isDeleting = false;
     };
-    TxtType.prototype.tick = function(){
+    TxtType.prototype.tick = function () {
         var i = this.loopNum % this.toRotate.length;
         var fullTxt = this.toRotate[i];
-        this.txt = this.isDeleting
-            ? fullTxt.substring(0, this.txt.length-1)
-            : fullTxt.substring(0, this.txt.length+1);
-
-        this.el.innerHTML = '<span class="wrap">'+this.txt+'</span>';
-
-        var delta = 200 - Math.random()*100;
-        if(this.isDeleting) { delta /= 2; }
-        if(!this.isDeleting && this.txt === fullTxt){
+        if (this.isDeleting) {
+            this.txt = fullTxt.substring(0, this.txt.length - 1);
+        } else {
+            this.txt = fullTxt.substring(0, this.txt.length + 1);
+        }
+        this.el.innerHTML = '<span class="wrap">' + this.txt + "</span>";
+        var that = this;
+        var delta = 200 - Math.random() * 100;
+        if (this.isDeleting) {
+            delta /= 2;
+        }
+        if (!this.isDeleting && this.txt === fullTxt) {
             delta = this.period;
             this.isDeleting = true;
-        } else if(this.isDeleting && this.txt === ''){
+        } else if (this.isDeleting && this.txt === "") {
             this.isDeleting = false;
             this.loopNum++;
             delta = 500;
         }
-        setTimeout(function(){ this.tick(); }.bind(this), delta);
+        setTimeout(function () {
+            that.tick();
+        }, delta);
     };
-
-    function setupTypewriter(){
-        var elements = document.getElementsByClassName('typewrite');
-        for(var i=0;i<elements.length;i++){
-            var toRotate = elements[i].getAttribute('data-type');
-            var period   = elements[i].getAttribute('data-period');
-            if(toRotate){
+    window.onload = function () {
+        var elements = document.getElementsByClassName("typewrite");
+        for (var i = 0; i < elements.length; i++) {
+            var toRotate = elements[i].getAttribute("data-type");
+            var period = elements[i].getAttribute("data-period");
+            if (toRotate) {
                 new TxtType(elements[i], JSON.parse(toRotate), period);
             }
         }
-    }
+    };
 
     // —— 5) jQuery hazır olunca tüm setup’ları çalıştır ——
     loadjQuery(function(){
